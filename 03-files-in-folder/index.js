d3ea92c00ba36c39f2180d76d0b78cb3984a1c6c
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-05-08 03:19:47 +0300

    feat: code index.js to 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout, exit } = process;
+
+fs.readdir(path.join(__dirname, '/secret-folder'), {withFileTypes: true}, (err, files) => {
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.stat(path.join(__dirname, '/secret-folder', file.name), (err, stats) => {
+        console.log(path.basename(file.name, path.extname(file.name)), 
+        '-', 
+        path.extname(file.name).slice(1), 
+        '-',
+        stats.size / 1024, 'kb');
+      });
+    }
+  })
+});
