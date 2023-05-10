382218bf3dc9f76c44ece872c717aafa3c28503b
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-05-10 14:28:49 +0300

    fix: fixed index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,34 @@
 const path = require('path');
 const fs = require('fs');
 
+fs.access(path.join(__dirname, 'files-copy'), (err) => {
+  if (!err) {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+      if (err) {
+        console.error('Error Found:', err);
+      }
+      copyDir();
+    });
+  } else {
+    copyDir();
+  }
+});
+
 function copyDir() {
   fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) {
-    return console.error('Error Found:', err);
+      return console.error('Error Found:', err);
     }
-  });
-  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, files) => {
-    files.forEach(file => {
-      fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
-        if (err) {
-          console.log('Error Found:', err);
-        }
-      });
-    })
+    fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, files) => {
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) {
+            console.error('Error Found:', err);
+          }
+        });
+      })
+    });
   });
 }
 
-copyDir();
+//copyDir();
